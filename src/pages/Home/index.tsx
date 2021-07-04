fee6cdf6313466a21f9388403ac0cd77817424a4
Gustavo Silva <gstvds@icloud.com>
2021-07-04 12:37:00 -0300

    feat: Adds Car card component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -6,6 +6,8 @@ import Logo from '../../assets/logo.svg';
 
 import { Container, Header, HeaderContent, TotalCars } from './styles';
 
+import { Car } from '../../components/Car';
+
 export function Home() {
   return (
     <Container>
@@ -18,6 +20,16 @@ export function Home() {
           </TotalCars>
         </HeaderContent>
       </Header>
+
+      <Car
+        brand="audi"
+        name="RS 5 Coupé"
+        thumbnail="https://freepngimg.com/thumb/audi/35227-5-audi-rs5-red.png"
+        rent={{
+          period: 'Ao dia',
+          price: 120,
+        }}
+      />
     </Container>
   );
 }
