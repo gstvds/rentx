0a97d3da0907a017e79ae1529b96b7e4be6f03bf
Gustavo Silva <gstvds@icloud.com>
2021-07-04 12:05:31 -0300

    feat: Adds HomePage structure and themes using styled-components

---
+++ src/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import AppLoading from 'expo-app-loading';
+import { ThemeProvider } from 'styled-components';
+
+import { useFonts, Archivo_400Regular, Archivo_500Medium, Archivo_600SemiBold } from '@expo-google-fonts/archivo';
+import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+
+import theme from './global/styles/theme';
+
+import { Home } from './pages/Home';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Archivo_400Regular,
+    Archivo_500Medium,
+    Archivo_600SemiBold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
+  );
+}
