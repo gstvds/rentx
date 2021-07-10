36eb077146cb312a300d6066eb0f77e3084e872b
Gustavo Silva <gstvds@icloud.com>
2021-07-10 15:17:57 -0300

    feat: Adds SplashScreen using expo

--- src/pages/Splash/index.tsx
+++ src/pages/Splash/index.tsx
@@ -1,39 +1,11 @@
 import React from 'react';
-import { Button, StyleSheet, Dimensions } from 'react-native';
-import Animated, { useSharedValue, useAnimatedStyle, withTiming, Easing } from 'react-native-reanimated';
 
 import { Container } from './styles';
 
-const WIDTH = Dimensions.get('window').width;
-
 export function Splash() {
-  const position = useSharedValue(0);
-
-  const animatedStyles = useAnimatedStyle(() => {
-    return {
-      transform: [
-        { translateX: withTiming(position.value, { duration: 500, easing: Easing.bezier(.01, 1.07, 1, -0.28)  }) }
-      ]
-    }
-  });
-
-  function handleAnimationPosition() {
-    position.value = Math.random() * (WIDTH - 100);
-  }
 
   return (
     <Container>
-      <Animated.View style={[styles.box, animatedStyles]} />
-
-      <Button title="Mover" onPress={handleAnimationPosition} />
     </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  box: {
-    width: 100,
-    height: 100,
-    backgroundColor: 'red',
-  },
-})
