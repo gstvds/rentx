ee8a49788d1c7b6616b5149cca14c34a92df66c6
Gustavo Silva <gstvds@icloud.com>
2021-07-10 17:02:27 -0300

    feat: Adds animation to MyCars floating button

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import { StatusBar } from 'react-native';
+import React, { useEffect, useState, useRef } from 'react';
+import { StatusBar, Dimensions } from 'react-native';
 import { RFValue } from 'react-native-responsive-fontsize';
 import { useNavigation } from '@react-navigation/native';
+import { RectButton, PanGestureHandler } from 'react-native-gesture-handler';
+
+import Animated, { useSharedValue, useAnimatedStyle, useAnimatedGestureHandler, withSpring } from 'react-native-reanimated';
+const AnimatedButton = Animated.createAnimatedComponent(RectButton);
 
 import { api } from '../../services/api';
 import { CarDTO } from '../../dtos/CarDTO';
@@ -15,8 +19,32 @@ import { Load } from '../../components/Load';
 
 export function Home() {
   const navigation = useNavigation();
+  const width = useRef(Dimensions.get('window').width).current;
   const [cars, setCars] = useState<CarDTO[]>([]);
   const [loading, setLoading] = useState(true);
+  const positionY = useSharedValue(0);
+  const positionX = useSharedValue(0);
+  const myCarsStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        { translateX: positionX.value },
+        { translateY: positionY.value },
+      ],
+    };
+  });
+  const onGestureEvent = useAnimatedGestureHandler({
+    onStart(_, context: any) {
+      context.positionX = positionX.value;
+      context.positionY = positionY.value;
+    },
+    onActive(event, context: any) {
+      positionX.value = context.positionX + event.translationX;
+      positionY.value = context.positionY + event.translationY;
+    },
+    onEnd() {
+      positionX.value = positionX.value >= (-width / 2) ? withSpring(22) : withSpring(-width + 82);
+    },
+  });
 
   function handleCarDetails(car: CarDTO) {
     navigation.navigate('CarDetails', { car });
@@ -70,9 +98,13 @@ export function Home() {
         )
       }
 
-      <MyCarsButton onPress={handleOpenMyCars}>
-        <MyCarsButtonIcon name="ios-car-sport" />
-      </MyCarsButton>
+      <PanGestureHandler onGestureEvent={onGestureEvent}>
+        <Animated.View style={myCarsStyle}>
+          <MyCarsButton as={AnimatedButton} onPress={handleOpenMyCars}>
+            <MyCarsButtonIcon name="ios-car-sport" />
+          </MyCarsButton>
+        </Animated.View>
+      </PanGestureHandler>
     </Container>
   );
 }
